6164b4bb0bdd42c3fb16694c44c7611f66026752
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2022-06-01 13:56:38 -0700

    Initial commit

---
+++ contracts/SticksNFT.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+
+contract SticksNFT is ERC721A, Ownable, ERC2981, ReentrancyGuard{
+    using Strings for uint256;
+    
+    uint256 public constant MAX_SUPPLY = 1000;
+    uint256 public constant MAX_PUBLIC_MINT = 2;
+
+    string private  baseTokenUri;
+    string public   placeholderTokenUri;
+
+    //toggle reveal, toggle Mint 
+    bool public isRevealed;
+    bool public publicSale;
+
+    mapping(address => uint256) public totalPublicMint;
+
+    constructor(uint96 _royaltyFeesInBips, string memory _hiddenURI) ERC721A("SticksNFT", "STK"){
+        setRoyaltyInfo(msg.sender, _royaltyFeesInBips);
+        placeholderTokenUri = _hiddenURI;
+    }
+
+    //Make sure that calls are not from contracts
+    modifier callerIsUser() {
+        require(tx.origin == msg.sender, "StickNFT - Cannot be called by a contract");
+        _;
+    }
+
+    function mint(uint256 _quantity) external payable callerIsUser{
+        require(publicSale, "StickNFT - Not Yet Active.");
+        require((totalSupply() + _quantity) <= MAX_SUPPLY, "StickNFT - Beyond Max Supply");
+        require((totalPublicMint[msg.sender] +_quantity) <= MAX_PUBLIC_MINT, "StickNFT - hit max mint for this wallet!");
+
+        totalPublicMint[msg.sender] += _quantity;
+        _safeMint(msg.sender, _quantity);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenUri;
+    }
+
+    //return uri for certain token
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        if(!isRevealed){
+            return placeholderTokenUri;
+        }
+  
+        return bytes(baseTokenUri).length > 0 ? string(abi.encodePacked(baseTokenUri, tokenId.toString(), ".json")) : "";
+    }
+
+    function tokensOfOwner(address _owner)
+        external
+        view
+        returns (uint256[] memory ownerTokens)
+    {
+        uint256 tokenCount = balanceOf(_owner);
+        if (tokenCount == 0) {
+            return new uint256[](0);
+        } else {
+            uint256[] memory result = new uint256[](tokenCount);
+            uint256 totalTkns = totalSupply();
+            uint256 resultIndex = 0;
+            uint256 tnkId;
+
+            for (tnkId = _startTokenId(); tnkId <= totalTkns; tnkId++) {
+                if (ownerOf(tnkId) == _owner) {
+                    result[resultIndex] = tnkId;
+                    resultIndex++;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    // method overriden to start token ID from 1.
+    function _startTokenId() internal pure virtual override returns (uint256) {
+        return 1;
+    }
+
+    //Interface overide for royalties
+     function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721A, ERC2981)
+        returns (bool)
+    {
+        return
+            super.supportsInterface(interfaceId);
+    }
+
+    //Only Owner Functions
+    function setRoyaltyInfo(address _receiver, uint96 _royaltyFeesInBips) public onlyOwner {
+        _setDefaultRoyalty(_receiver, _royaltyFeesInBips);
+    }
+
+    function setTokenUri(string memory _baseTokenUri) external onlyOwner{
+        baseTokenUri = _baseTokenUri;
+    }
+    function setPlaceHolderUri(string memory _placeholderTokenUri) external onlyOwner{
+        placeholderTokenUri = _placeholderTokenUri;
+    }
+
+    function togglePublicSale() external onlyOwner{
+        publicSale = !publicSale;
+    }
+
+    function toggleReveal() external onlyOwner{
+        isRevealed = !isRevealed;
+    }
+
+    function withdraw() external onlyOwner nonReentrant{
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+const hre = require("hardhat");
+
+async function main() {
+
+  //variables
+  const royalty = "500";
+  const hiddenURI = "https://nfbeez.mypinata.cloud/ipfs/QmSnQ8qXZX2ADbiYni9fJ4igyTDHgF9Q7HZweFb7BHTUuq/1.json";
+ // const merkle = "";
+  
+  const Web3 = await hre.ethers.getContractFactory("SticksNFT");
+  const web3 = await Web3.deploy(royalty, hiddenURI);
+
+  await web3.deployed();
+
+  console.log("SticksNFT Contract deployed to:", web3.address);
+  const receipt = await web3.deployTransaction.wait();
+  console.log("gasUsed:" , receipt.gasUsed);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
